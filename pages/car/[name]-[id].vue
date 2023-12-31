bc18b375c6a68f4c00db72b967b5926e91dd1e6c
Charlie <charlie.chuangqi.li@gmail.com>
2023-12-31 18:51:43 +1000

    error handling

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,18 +1,30 @@
 <script setup>
 const route = useRoute()
+const { cars } = useCars()
 const { toTitleCase } = useUtilities()
 useHead({
     title: toTitleCase(route.params.name)
 })
+const car = computed(() => {
+    return cars.find((c) => {
+        return c.id === parseInt(route.params.id);
+    })
+})
+if (!car.value) {
+    throw createError({
+        statusCode: 404,
+        message: `Car with ID of ${route.params.id} does not exist.`
+    })
+}
 definePageMeta({
     layout: "custom"
 })
 </script>
 <template>
     <div>
-        <CarDetailHero />
-        <CarDetailAttributes />
-        <CarDetailDescription />
+        <CarDetailHero :car="car" />
+        <CarDetailAttributes :features="car.features" />
+        <CarDetailDescription :description="car.description" />
         <CarDetailContact />
     </div>
 </template>
